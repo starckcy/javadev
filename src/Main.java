191598e017f3709eb00ca342c6f5920162ddf416
ayushkumarrai-starckcy <thisisayushkumarrai@gmail.com>
2026-06-25 22:02:56 +0530

    starting java development

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+        int num = 3;
+        System.out.println("nice to meet you " + num);
+
+        int x = 32;
+        double y = 3.14;
+        char ch = 'a';
+        boolean b = true;
+
+        String str = "abc";
+        System.out.println(str + "de");
+    }
+}
