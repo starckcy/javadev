aaafef733b1fd242dd4a1e2d443a87e375b366b4
ayushkumarrai-starckcy <thisisayushkumarrai@gmail.com>
2026-06-25 22:57:42 +0530

    using scanner to take user input

--- src/userInput.java
+++ src/userInput.java
@@ -1,9 +1,31 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class userInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+//        System.out.print("Enter your name: ");
+//        String name = sc.nextLine();
+//
+//        System.out.print("Enter your age: ");
+//        int age = sc.nextInt();
+//
+//        System.out.println("hello " + name);
+//        System.out.println("you are " + age + " years old");
+
+//        common issues
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        sc.nextLine(); // to avoid sc taking /n as input
+
+        System.out.print("what is your favorite color? ");
+        String color = sc.nextLine();
+
+        System.out.println("your are " + age + " years old");
+        System.out.println("your favorite color is " + color);
+
+
         sc.close();
     }
 }
